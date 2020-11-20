e6b26a8501cbc953fe8a11c12442de538cc0bf07
Natalie Huante <huante@chapman.edu>
2020-11-20 11:42:07 -0800

    edited username.sh

--- username.sh
+++ username.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 # username.sh
 # Natalie Huante
-echo "Username can contain lowercase letters, underscores, and digits.\n It must start with a lowercase letter and be a length of 3-12"
+echo "Username can contain lowercase letters, underscores, and digits."
+echo "It must start with a lowercase letter and be a length of 3-12"
 
 echo "Enter a username: "
 read -r NAME
 
 while echo "$NAME" | grep -E -v "^[a-z][0-9a-z_]{2,11}$" > /dev/null 2>&1
 do
-	echo "Invalid username!"
+	echo "Invalid entry!"
 	echo "Enter a username: "
 	read -r NAME
 done
